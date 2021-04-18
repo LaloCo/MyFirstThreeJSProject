1794de43c43957c5159dd616cbf568f64ebf819e
Eduardo Rosas <lalorosas@outlook.com>
2021-04-18 11:16:07 -0500

    Display a red cube

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello Three.js!!!")
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1) // width, height, depth
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const cube = new THREE.Mesh(geometry, material)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // degrees & aspect ratio
+camera.position.z = 3 // three times thee size of cube (which is 1x1x1 as set)
+camera.position.x = 1
+camera.position.y = 1
+
+scene.add(cube)
+scene.add(camera)
+
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
